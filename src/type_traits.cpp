bff0a5a84f1a134e37ed4c7e52607a2c848b95e5
nikolas <nikolas@nikolas-MacBook-Pro.local>
2026-08-12 17:38:00 +0200

    removed wip type_traits from source files

--- src/type_traits.cpp
+++ src/type_traits.cpp
@@ -8,22 +8,21 @@ import :integers;
 import :boolean;
 import :decimals;
 
-#define CREATE_TYPE_TRAITS_TEMPLATE_FOR_HELIUM_TYPE(HeType)                                    \
-    template<typename T>                                                                       \
-    class numeric_limits<HeType<T>> {                                                          \
-    public:                                                                                    \
-        static constexpr HeType<T> min() { return std::numeric_limits<T>::min(); }             \
-        static constexpr HeType<T> max() { return std::numeric_limits<T>::max(); }             \
-\
-        static constexpr bool is_real = std::numeric_limits<T>::is_real;  \
-        static constexpr bool is_signed = std::numeric_limits<T>::is_signed; \
-        static constexpr bool is_integer = std::numeric_limits<T>::is_integer; \
-                                                                                               \
-        static bool is_specialized() { return true; }                                          \
-    };  \
+#define CREATE_TYPE_TRAITS_TEMPLATE_FOR_NH_TYPE(NHType)                        \
+  template <typename T> class numeric_limits<NHType<T>> {                      \
+  public:                                                                      \
+    static constexpr NHType<T> min() { return std::numeric_limits<T>::min(); } \
+    static constexpr NHType<T> max() { return std::numeric_limits<T>::max(); } \
+                                                                               \
+    static constexpr bool is_real = std::numeric_limits<T>::is_real;           \
+    static constexpr bool is_signed = std::numeric_limits<T>::is_signed;       \
+    static constexpr bool is_integer = std::numeric_limits<T>::is_integer;     \
+                                                                               \
+    static bool is_specialized() { return true; }                              \
+  };
 
 export namespace std {
 
-//CREATE_TYPE_TRAITS_TEMPLATE_FOR_HELIUM_TYPE(he::SafeSignedInt)
-//CREATE_TYPE_TRAITS_TEMPLATE_FOR_HELIUM_TYPE(he::FastInt)
+// CREATE_TYPE_TRAITS_TEMPLATE_FOR_NH_TYPE(nh::SafeSignedInt)
+// CREATE_TYPE_TRAITS_TEMPLATE_FOR_NH_TYPE(nh::FastInt)
 }
--- tests/test_integer.cpp
+++ tests/test_integer.cpp
@@ -9,132 +9,125 @@ import nhtypes;
 
 using namespace nh;
 
-TEST_CASE("Convert between integer types")
-{
-    SECTION("Signed to signed")
-    {
-        FastI8  fi8   = FastI8();
-        FastI16 fi16  = FastI8();
-        FastI32 fi32  = FastI16();
-        FastI64 fi64  = FastI32();
-        FastI64 fi64_ = FastI64();
+TEST_CASE("Convert between integer types") {
+  SECTION("Signed to signed") {
+    FastI8 fi8 = FastI8();
+    FastI16 fi16 = FastI8();
+    FastI32 fi32 = FastI16();
+    FastI64 fi64 = FastI32();
+    FastI64 fi64_ = FastI64();
 
-        SafeI8  si8   = FastI8();
-        SafeI16 si16  = FastI8();
-        SafeI32 si32  = FastI16();
-        SafeI64 si64  = FastI32();
-        SafeI64 si64_ = FastI64();
+    SafeI8 si8 = FastI8();
+    SafeI16 si16 = FastI8();
+    SafeI32 si32 = FastI16();
+    SafeI64 si64 = FastI32();
+    SafeI64 si64_ = FastI64();
 
-        SafeI8  si8_    = SafeI8();
-        SafeI16 si16_   = SafeI8();
-        SafeI32 si32_   = SafeI16();
-        SafeI64 si64__  = SafeI32();
-        SafeI64 si64___ = SafeI64();
-    }
+    SafeI8 si8_ = SafeI8();
+    SafeI16 si16_ = SafeI8();
+    SafeI32 si32_ = SafeI16();
+    SafeI64 si64__ = SafeI32();
+    SafeI64 si64___ = SafeI64();
+  }
 
-    SECTION("Unsigned to unsigned")
-    {
-        // Same classes as signed counterparts(only internal type is different)
-        // and therefore identical behaviour as above.
-    }
+  SECTION("Unsigned to unsigned") {
+    // Same classes as signed counterparts(only internal type is different)
+    // and therefore identical behaviour as above.
+  }
 
-    SECTION("Unsigned to signed")
-    {
-        FastI16 fi16 = FastU8();
-        FastI32 fi32 = FastU8();
-        FastI64 fi64 = FastU32();
+  SECTION("Unsigned to signed") {
+    FastI16 fi16 = FastU8();
+    FastI32 fi32 = FastU8();
+    FastI64 fi64 = FastU32();
 
-        SafeI16 si16 = FastU8();
-        SafeI32 si32 = FastU16();
-        SafeI64 si64 = FastU32();
+    SafeI16 si16 = FastU8();
+    SafeI32 si32 = FastU16();
+    SafeI64 si64 = FastU32();
 
-        SafeI16 si16_ = SafeU8();
-        SafeI32 si32_ = SafeU16();
-        SafeI64 si64_ = SafeU32();
-    }
+    SafeI16 si16_ = SafeU8();
+    SafeI32 si32_ = SafeU16();
+    SafeI64 si64_ = SafeU32();
+  }
 }
 
-#define Integer_Arithmetic_Tests(Type, CType) \
-                                              \
-    REQUIRE_NOTHROW(Type(Min));               \
-    REQUIRE_NOTHROW(Type(Max));               \
-                                              \
-    REQUIRE_THROWS(Type(Min) -= 1);           \
-    REQUIRE_THROWS(Type(Max) += 1);           \
-    REQUIRE_THROWS(Type(Max) *= 2);           \
-    REQUIRE_THROWS(Type(Max) /= 0);           \
-                                              \
-    REQUIRE_NOTHROW(Type(Min) += 1);          \
-    REQUIRE_NOTHROW(Type(Max) -= 1);          \
-    REQUIRE_NOTHROW(Type(1) *= 2);            \
-    REQUIRE_NOTHROW(Type(Max) /= 1);          \
-                                              \
-    REQUIRE_NOTHROW(Type(Min)++);             \
-    REQUIRE_NOTHROW(++Type(Min));             \
-    REQUIRE_NOTHROW(Type(Max)--);             \
-    REQUIRE_NOTHROW(--Type(Max));             \
-                                              \
-    REQUIRE_THROWS(Type(Max)++);              \
-    REQUIRE_THROWS(++Type(Max));              \
-    REQUIRE_THROWS(Type(Min)--);              \
-    REQUIRE_THROWS(--Type(Min));              \
-                                              \
-    REQUIRE(+Type(1));                        \
-    REQUIRE_FALSE(+Type(0));
+#define Integer_Arithmetic_Tests(Type, CType)                                  \
+                                                                               \
+  REQUIRE_NOTHROW(Type(Min));                                                  \
+  REQUIRE_NOTHROW(Type(Max));                                                  \
+                                                                               \
+  REQUIRE_THROWS(Type(Min) -= 1);                                              \
+  REQUIRE_THROWS(Type(Max) += 1);                                              \
+  REQUIRE_THROWS(Type(Max) *= 2);                                              \
+  REQUIRE_THROWS(Type(Max) /= 0);                                              \
+                                                                               \
+  REQUIRE_NOTHROW(Type(Min) += 1);                                             \
+  REQUIRE_NOTHROW(Type(Max) -= 1);                                             \
+  REQUIRE_NOTHROW(Type(1) *= 2);                                               \
+  REQUIRE_NOTHROW(Type(Max) /= 1);                                             \
+                                                                               \
+  REQUIRE_NOTHROW(Type(Min)++);                                                \
+  REQUIRE_NOTHROW(++Type(Min));                                                \
+  REQUIRE_NOTHROW(Type(Max)--);                                                \
+  REQUIRE_NOTHROW(--Type(Max));                                                \
+                                                                               \
+  REQUIRE_THROWS(Type(Max)++);                                                 \
+  REQUIRE_THROWS(++Type(Max));                                                 \
+  REQUIRE_THROWS(Type(Min)--);                                                 \
+  REQUIRE_THROWS(--Type(Min));                                                 \
+                                                                               \
+  REQUIRE(+Type(1));                                                           \
+  REQUIRE_FALSE(+Type(0));
 
-#define Integer_Bitwise_Tests(Type, CType)                \
-    REQUIRE_THROWS(Type(0) <<= (sizeof(CType) * 8));      \
-    REQUIRE_THROWS(Type(0) >>= (sizeof(CType) * 8));      \
-                                                          \
-    REQUIRE_NOTHROW(Type(0) <<= (sizeof(CType) * 8) - 1); \
-    REQUIRE_NOTHROW(Type(0) <<= 0);                       \
-    REQUIRE_NOTHROW(Type(0) >>= (sizeof(CType) * 8) - 1); \
-    REQUIRE_NOTHROW(Type(0) >>= 0);
+#define Integer_Bitwise_Tests(Type, CType)                                     \
+  REQUIRE_THROWS(Type(0) <<= (sizeof(CType) * 8));                             \
+  REQUIRE_THROWS(Type(0) >>= (sizeof(CType) * 8));                             \
+                                                                               \
+  REQUIRE_NOTHROW(Type(0) <<= (sizeof(CType) * 8) - 1);                        \
+  REQUIRE_NOTHROW(Type(0) <<= 0);                                              \
+  REQUIRE_NOTHROW(Type(0) >>= (sizeof(CType) * 8) - 1);                        \
+  REQUIRE_NOTHROW(Type(0) >>= 0);
 
-#define Make_Signed_Integer_Tests(Type, CType)                   \
-    TEST_CASE("Test " #Type)                                     \
-    {                                                            \
-        constexpr CType Min = std::numeric_limits<CType>::min(); \
-        constexpr CType Max = std::numeric_limits<CType>::max(); \
-                                                                 \
-        Integer_Arithmetic_Tests(Type, CType);                   \
-                                                                 \
-        REQUIRE_THROWS(Type(Min) *= 2);                          \
-        REQUIRE_THROWS(Type(Max) -= (-1));                       \
-        REQUIRE_THROWS(-Type(Min));                              \
-    }
+#define Make_Signed_Integer_Tests(Type, CType)                                 \
+  TEST_CASE("Test " #Type) {                                                   \
+    constexpr CType Min = std::numeric_limits<CType>::min();                   \
+    constexpr CType Max = std::numeric_limits<CType>::max();                   \
+                                                                               \
+    Integer_Arithmetic_Tests(Type, CType);                                     \
+                                                                               \
+    REQUIRE_THROWS(Type(Min) *= 2);                                            \
+    REQUIRE_THROWS(Type(Max) -= (-1));                                         \
+    REQUIRE_THROWS(-Type(Min));                                                \
+  }
 
-#define Make_Unsigned_Integer_Tests(Type, CType)                 \
-    TEST_CASE("Test " #Type)                                     \
-    {                                                            \
-        constexpr CType Min = std::numeric_limits<CType>::min(); \
-        constexpr CType Max = std::numeric_limits<CType>::max(); \
-                                                                 \
-        Integer_Arithmetic_Tests(Type, CType);                   \
-                                                                 \
-        REQUIRE_NOTHROW(Type(Min) *= 2);                         \
-    }
+#define Make_Unsigned_Integer_Tests(Type, CType)                               \
+  TEST_CASE("Test " #Type) {                                                   \
+    constexpr CType Min = std::numeric_limits<CType>::min();                   \
+    constexpr CType Max = std::numeric_limits<CType>::max();                   \
+                                                                               \
+    Integer_Arithmetic_Tests(Type, CType);                                     \
+                                                                               \
+    REQUIRE_NOTHROW(Type(Min) *= 2);                                           \
+  }
 
-#define Make_Index_Integer_Tests(Type, CType)                    \
-    TEST_CASE("Test " #Type)                                     \
-    {                                                            \
-        constexpr CType Min = std::numeric_limits<CType>::min(); \
-        constexpr CType Max = std::numeric_limits<CType>::max(); \
-                                                                 \
-        Integer_Arithmetic_Tests(Type, CType);                   \
-        Integer_Bitwise_Tests(Type, CType);                      \
-                                                                 \
-        REQUIRE_NOTHROW(Type(Min) *= 2);                         \
-        REQUIRE(Type(1));                                        \
-        REQUIRE_FALSE(Type(0));                                  \
-    }
+#define Make_Index_Integer_Tests(Type, CType)                                  \
+  TEST_CASE("Test " #Type) {                                                   \
+    constexpr CType Min = std::numeric_limits<CType>::min();                   \
+    constexpr CType Max = std::numeric_limits<CType>::max();                   \
+                                                                               \
+    Integer_Arithmetic_Tests(Type, CType);                                     \
+    Integer_Bitwise_Tests(Type, CType);                                        \
+                                                                               \
+    REQUIRE_NOTHROW(Type(Min) *= 2);                                           \
+    REQUIRE(Type(1));                                                          \
+    REQUIRE_FALSE(Type(0));                                                    \
+  }
 
-    Make_Unsigned_Integer_Tests(SafeU8, uint8_t);
-    Make_Unsigned_Integer_Tests(SafeU16, uint16_t);
-    Make_Unsigned_Integer_Tests(SafeU32, uint32_t);
-    Make_Unsigned_Integer_Tests(SafeU64, uint64_t);
+Make_Unsigned_Integer_Tests(SafeU8, uint8_t);
+Make_Unsigned_Integer_Tests(SafeU16, uint16_t);
+Make_Unsigned_Integer_Tests(SafeU32, uint32_t);
+Make_Unsigned_Integer_Tests(SafeU64, uint64_t);
 
-    Make_Signed_Integer_Tests(SafeI8, int8_t);
-    Make_Signed_Integer_Tests(SafeI16, int16_t);
-    Make_Signed_Integer_Tests(SafeI32, int32_t);
-    Make_Signed_Integer_Tests(SafeI64, int64_t);
+Make_Signed_Integer_Tests(SafeI8, int8_t);
+Make_Signed_Integer_Tests(SafeI16, int16_t);
+Make_Signed_Integer_Tests(SafeI32, int32_t);
+Make_Signed_Integer_Tests(SafeI64, int64_t);
